9a63273cfcf16868e63c8289791fcbed4fb6c736
Simara08 <7knywpc@code.edu.az>
2022-03-17 10:03:12 +0400

    created proje1

---
+++ method1/method1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace method1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 8, 2, 5, 7 };
+            Console.WriteLine("Massivin en boyuk elementi");
+            
+            Console.WriteLine(Array(num));
+        }
+    
+    
+    static int Array(int[] a)
+        {
+            int max = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (max<a[i])
+                {
+                    max = a[i];
+                }
+            }
+            return max;
+        }
+    
+    
+    }
+}
